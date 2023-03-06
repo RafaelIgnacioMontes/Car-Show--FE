35c066c4978e963af0c037eb959450113e456dfd
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-03-06 08:16:40 -0700

    car component in progress

--- car-show/src/components/Car.jsx
+++ car-show/src/components/Car.jsx
@@ -1,3 +1,38 @@
-const Car = () => {}
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Car = (response) => {
+  const [carList, setCarList] = useState([])
+  const [comments, setComments] = useState([])
+  const getAllCars = async () => {
+    const response = await axios.get('http://localhost:3001/car/all')
+    setCarList(response.data.cars)
+  }
+  const getAllComments = async () => {
+    const response = await axios.get('http://localhost:3001/comment/all')
+    setComments(response.data.comments)
+  }
+
+  useEffect(() => {
+    getAllCars()
+  }, [])
+  return (
+    <div>
+      <h1 className="Title"> Car Collection </h1>
+
+      {carList.map((cars) => (
+        <div className="Car-Card">
+          <img src={rides.image} alt="car image" />
+          <p className="make">Make:{cars.make}</p>
+          <p className="model">Model:{cars.model}</p>
+          <p className="year">Year Built:{cars.year}</p>
+          <p className="vin">VIN: {cars.vin}</p>
+          <p className="color">Color: {cars.color}</p>
+          <div></div>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 export default Car
