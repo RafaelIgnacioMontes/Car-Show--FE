8fbfe33ad6011be80d8788474dfc027359397db2
Michael Durando <mjdurando82@yahoo.com>
2023-03-06 10:55:12 -0500

    merging with  main to include services

--- car-show/src/App.js
+++ car-show/src/App.js
@@ -1,12 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Nav from './components/Nav'
 import Home from './components/Home'
 import Register from './components/Register'
-
-import Home from './components/Home'
 
 function App() {
   return (
