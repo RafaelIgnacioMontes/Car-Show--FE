a535e30bc72daf949d44016c5ddd6307bbc0821a
Clifton Lucas <cliftonlucas89@gmail.com>
2023-03-06 09:38:14 -0600

    header, home and css updates

--- car-show/src/App.js
+++ car-show/src/App.js
@@ -1,9 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 import './App.css'
+
+import Home from './components/Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </div>
   )
 }
---
+++ car-show/src/components/Home.jsx
@@ -0,0 +1,12 @@
+// import { useState, useEffect } from 'react'
+// import { Link } from 'react-router-dom'
+// import axios from 'axios'
+
+
+const Home = () => {
+  return(
+  <div>Home Page</div>
+  )
+}
+
+export default Home
