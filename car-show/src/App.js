4d371bd1da078d94fb55a97867c685b4ff82bf72
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-03-07 12:16:05 -0700

    stuck on findbypk

--- car-show/src/App.js
+++ car-show/src/App.js
@@ -9,14 +9,14 @@ import SignIn from './components/SignIn'
 import { CheckSession } from './services/Auth'
 import User from './components/User'
 import axios from 'axios'
-
 import CarDetails from './components/CarDetails'
 
 const App = () => {
   let navigate = useNavigate()
+
   const [userCarList, setUserCarList] = useState([])
 
-  const [carDetails, setCarDetails] = useState([])
+  const [carDetails, setCarDetails] = useState()
 
   const [user, setUser] = useState(null)
 
@@ -35,42 +35,30 @@ const App = () => {
     setUser(user)
   }
 
-  const getToken = async () => {
-    token = await localStorage.getItem('token')
-  }
   useEffect(() => {
     const token = localStorage.getItem('token')
     if (token) {
       checkToken()
     }
   }, [])
-  const getUsersCars = async () => {
-    const cars = await axios.get(`http://localhost:3001/cars/user/${user.id}`)
-
-    setUserCarList(cars.data)
+  const getUsersCars = async (props) => {
+    const carsss = await axios.get(`http://localhost:3001/cars/user/${user.id}`)
   }
 
   const getAllCars = async () => {
     const response = await axios.get('http://localhost:3001/cars/all')
     setCarList(response.data)
-    console.log(response)
+    console.log(response.data)
   }
   const getAllComments = async () => {
     const response = await axios.get('http://localhost:3001/comment/all')
     setComments(response.data)
     // console.log(response)
   }
-  const getCarDetails = async (params) => {
-    const response = await axios.get(
-      `http://localhost:3001/cars/car/${params.car_id}`
-    )
-    setCarDetails(response)
-    console.log(response)
-  }
-
-  const handleClick = () => {
-    navigate('/CarDetails/:car_id')
-  }
+  // const getCarDetails = async () => {
+  //   const carDeets = await axios.get(`http://localhost:3001/cars/car/${}`)
+  //   setCarDetails(carDeets)
+  // }
 
   console.log(carList)
   useEffect(() => {
@@ -113,8 +101,8 @@ const App = () => {
           <Route path="/signIn/" element={<SignIn setUser={setUser} />} />
           <Route path="/about" element={<About />} />
           <Route
-            path="/CarDetails/:car_id"
-            element={<CarDetails getCarDetails={getCarDetails} />}
+            path={`/:id/CarDetails`}
+            element={<CarDetails carDetails={carDetails} />}
           />
         </Routes>
       </main>
